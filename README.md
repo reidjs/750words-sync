5e564281ce52c567d5709ed370051271cf4a402c
Reid Sherman <reidsherman@gmail.com>
2020-05-27 12:54:00 -0700

    update sync script

--- README.md
+++ README.md
@@ -1,57 +1,47 @@
 # 750Words Sync
+Automate archiving and uploading text to 750words.com. 
+
+
+## SETUP
+### 1. Download
+1. Open a terminal
+
+2. Clone this project
+
+`git clone https://github.com/reidjs/750words-sync.git`
+
+3. Change directory (cd) to this project
+
+`cd 750words-sync`
+
+4. Enable the sync script
+
+`chmod +x sync.sh`
+
+5. (OPTIONAL) set the sync script to run automatically every minute
+
+`crontab -e`
+
+Add this line to your crontab: (if it opened in vim, press `i` then paste with CTRL+V)
+
+`* * * * * (cd /Users/reidjs/Projects/750words-sync/; ./sync.sh)`
+
+Write and save (press `esc` then `:wq!` if in vim)
+
+### Configure .env
+1. Open EMPTY.env and set NAME and PASSWORD to your 750words.com login:
+NAME
+PASSWORD
+
+
 ## TODO
 - (advertise) Set up alerts for when 750 words is mentioned online (reddit, hn) and pitch this project
 
-configure .env
-NAME
-PASSWORD
-DIRECTORY
 
 /usr/local/bin
 
 ## Normal Sync
-Run SYNC.sh and leave it open in the background
-Edit TODAY.md and save it.
-You should see a success message 
-
-- At Midnight rename `TODAY.md` to today's date `March 4, 2020.md` and move it to `Archive/`
-- Generate a new file, `TODAY.md`, to be edited in the directory of your choice
-- When `TODAY.md` changes (and saved), run index.js to sync to website
-  - use entr to watch the file for changes
 
 - Write explanation
 - Write setup guide
 - Write how it works
-
-## Auto-sync
-
-# watchman
-
-`brew update`
-`brew install watchman`
-
-
-
-watchman-make -p 'TODAY.md' --run foo.sh
-
-foo.sh is in /bin/sh, might have to use full path
-
-watchman-make -p 'TODAY.md' --run '/Users/reidjs/Projects/750words-api/foo2.sh'
-
-run in background (doesn't work after logout):
-[from the project dir]
-`nohup watchman-make -p 'TODAY.md' --run '/Users/reidjs/Projects/750words-api/foo2.sh' &`
-
----
-
-
-entr
-homebrew: `brew install entr`
-  - how to install homebrew
-
-or direct install and walk through the entr README
-
-run entr in the background
-`ls TODAY.md | entr -n echo 'hiasdf' > text.txt &`
-
----
--- sync.sh
+++ sync.sh
@@ -1,51 +1,54 @@
 #!/bin/bash
-# Move this file to /usr/local/bin/
+# Move this file to /usr/local/bin/?
 # Set cron to run every x seconds/hours/days
-
-# https://stackoverflow.com/questions/16391208/print-a-files-last-modified-date-in-bash
-# Store last time file modified in file (date -r)
-# If TODAY.md has been modified since last time script run, run JS file to update changes to website (index.js)
-# If today date is after date of file creation, mv TODAY.md to Archive Dir and rename it to today's date "March 1, 2020.md", Create a new TODAY.md in folder
 
 lastSyncFile=".last-sync"
 if [ ! -f "$lastSyncFile" ]; then
   touch $lastSyncFile
 fi
-# First line of file
+# Grab first line of sync record file
 lastSyncDate=$(head -n 1 ${lastSyncFile})
-# 2nd line of file
-lastSyncTime=$(sed -n '2p' < ${lastSyncFile})
+# Grab second line of sync record file
+lastEditTime=$(sed -n '2p' < ${lastSyncFile})
 
 TODAY=$(date +"%b %d, %Y")
 NOW=$(date)
-# file is empty, so set last sync date to now
+lastTimeWorkFileModified=$(date -r TODAY.md)
 
 function updateSyncFile {
   lastSyncDate=$TODAY
-  lastSyncTime=$NOW
+  lastEditTime=$lastTimeWorkFileModified
+
   echo "${lastSyncDate}" > "${lastSyncFile}"
-  echo "${lastSyncTime}" >> "${lastSyncFile}"
+  echo "${lastEditTime}" >> "${lastSyncFile}"
 }
 
-if [ -z "$lastSyncDate" ] || [ -z "$lastSyncTime" ]; then
+echo "Attempting sync ${NOW}" >> .log
+
+# If work file not modified since last sync, exit
+if [ "$lastEditTime" == "$lastTimeWorkFileModified" ]; then
+  echo "No need to run sync, file not editted" >> .log
+  echo "" >> .log
+  exit 1
+fi
+
+# Was not able to set these variables, so update the sync file
+if [ -z "$lastSyncDate" ] || [ -z "$lastEditTime" ]; then
   updateSyncFile
 fi
 
-# echo "previous sync date: ${lastSyncDate}"
-echo "Last sync: ${lastSyncTime}" >> .log
-
-# fancyDate=$(date +"%b %d, %Y")
-# echo "fancy date: ${fancyDate}"
+echo "Last sync date: ${lastSyncDate}" >> .log
 
 archiveFile="ARCHIVE/${TODAY}.md"
 
-# copy text from today's doc to archive file 
+# copy text from working file to archive file
 cp TODAY.md "${archiveFile}"
 
 echo "Archived today's work into ${archiveFile}" >> .log
 
-# TODO: sync text in TODAY.md to 750words.com
-npm start
+# Sync text from working file to 750words.com
+NODEDIR=$(which node)
+$NODEDIR uploadText.js
 echo "Sync'd today's work to 750words.com" >> .log
 echo "" >> .log
 
